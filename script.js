b661a9780c5c8886a83b202c9548913a9161db53
Chanuka-dissanayaka <Chanukaati001@gmail.com>
2025-04-13 19:53:21 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/* ==================== typing animation =====================*/
+window.onload = function () {
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Computer Hardware Technician", "Solar Photovoltaic System Technician", "Network Technician"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+};
